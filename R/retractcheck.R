cea4443417ecfcbc18361298bc06b677f1dee0bd
Chris Hartgerink <chris@libscie.org>
2018-03-29 13:26:26 +0200

    Vectorize retractcheck()

--- R/retractcheck.R
+++ R/retractcheck.R
@@ -1,31 +1,36 @@
-retractcheck <- function (doi) {
-  if (!check_doi(doi)) stop('Please input valid DOI.')
+retractcheck <- function (dois) {
+  listdf <- apply(t(dois), 2, function (doi) {
+    if (!check_doi(doi)) stop('Please input valid DOI.')
 
-  call <- httr::GET(construct_url(doi))
+    call <- httr::GET(construct_url(doi))
 
-  if (call$status_code == 404) {
-    message(sprintf('No updates found for %s', doi))
-  } else {
-    obj <- httr::content(call)
+    if (call$status_code == 404) {
+      message(sprintf('No updates found for %s', doi))
+    } else {
+      obj <- httr::content(call)
 
-    published_original <- as.Date(as.POSIXct(obj$timestamp / 1000,
-     origin='1970-01-01'))
-    published_update <- as.Date(as.POSIXct(obj$updates[[1]]$timestamp / 1000,
-     origin='1970-01-01'))
-    update_delay <- difftime(published_update, published_original)
+      published_original <- as.Date(as.POSIXct(obj$timestamp / 1000,
+       origin='1970-01-01'))
+      published_update <- as.Date(as.POSIXct(obj$updates[[1]]$timestamp / 1000,
+       origin='1970-01-01'))
+      update_delay <- difftime(published_update, published_original)
 
-    res <- data.frame(doi, 
-      update_type = obj$updates[[1]]$type,
-      retracted = obj$retracted,
-      update_doi = obj$updates[[1]]$identifier$doi,
-      publisher = obj$publisher,
-      title = obj$title,
-      published_original,
-      published_update,
-      update_delay)
-  }
+      res <- data.frame(doi, 
+        update_type = obj$updates[[1]]$type,
+        retracted = obj$retracted,
+        update_doi = obj$updates[[1]]$identifier$doi,
+        publisher = obj$publisher,
+        title = obj$title,
+        published_original,
+        published_update,
+        update_delay)
+    }
 
-  return(res)
+    return(res)
+  })
+  df <- plyr::ldply(listdf, data.frame)
+
+  return(df)
 }
 
 retractcheck_textfile <- function (file) {
