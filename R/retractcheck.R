92ab8d280812e87c4a0ddd0a48f028114f795c19
Chris Hartgerink <chris@libscie.org>
2018-04-03 12:55:29 +0200

    Update for initial testing

--- R/retractcheck.R
+++ R/retractcheck.R
@@ -1,3 +1,27 @@
+#' retractcheck: Retraction scanner
+#'
+#' Using 'Digital Object Identifiers', check for retracted (or otherwise 
+#' updated) articles using 'Open Retractions' <http://openretractions.com>.
+#'
+#' @docType package
+#' @name retractcheck
+NULL
+
+#' Check DOIs for retractions
+#' 
+#' Using \url{http://openretractions.com} their API, this function 
+#' checks whether a DOI is updated, when that update was made, and what 
+#' type of update was made. DOIs without updates are not returned.
+#' 
+#' @param dois Vector of strings containing only DOIs
+#' 
+#' @return \code{\link{retractcheck}} dataframe
+#' @export
+#' @examples \dontrun{
+#'   retractcheck(c('10.1002/job.1787', 
+#'                  '10.1111/j.1365-2044.2012.07128.x'))
+#' }
+
 retractcheck <- function (dois) {
   listdf <- apply(t(dois), 2, function (doi) {
     if (!check_doi(doi)) {
@@ -39,40 +63,102 @@ retractcheck <- function (dois) {
   }
 }
 
-retractcheck_dir <- function (path, ...) {
-  text <- textreadr::read_dir(path, ...)$content
+#' Check files in directory for retractions
+#' 
+#' Check all HTML, DOCX, PDF, and RTF files in a directory for updates to 
+#' referenced DOIs.
+#' 
+#' @param path Path to directory to check
+#' 
+#' @return \code{\link{retractcheck}} dataframe
+#' @export
+#' @examples \dontrun{
+#'   retractcheck_dir(path = '.')
+#' }
+
+
+retractcheck_dir <- function (path) {
+  text <- textreadr::read_dir(path)$content
   dois <- find_doi(text)
   res <- retractcheck(dois)
 
   return(res)
 }
 
-retractcheck_docx <- function (path, ...) {
-  text <- textreadr::read_docx(path, ...)
+#' Check docx file for retractions
+#' 
+#' Check a DOCX file for retractions.
+#' 
+#' @param path Path to DOCX file to check
+#' 
+#' @return \code{\link{retractcheck}} dataframe
+#' @export
+#' @examples \dontrun{
+#'   retractcheck_docx('manuscript.docx')
+#' }
+
+retractcheck_docx <- function (path) {
+  text <- textreadr::read_docx(path)
   dois <- find_doi(text)
   res <- retractcheck(dois)
 
   return(res) 
 }
 
-retractcheck_pdf <- function (path, ...) {
-  text <- textreadr::read_pdf(path, ...)
+#' Check pdf file for retractions
+#' 
+#' Check a pdf file for retractions.
+#' 
+#' @param path Path to pdf file to check
+#' 
+#' @return \code{\link{retractcheck}} dataframe
+#' @export
+#' @examples \dontrun{
+#'   retractcheck_pdf('manuscript.pdf')
+#' }
+
+retractcheck_pdf <- function (path) {
+  text <- textreadr::read_pdf(path)
   dois <- find_doi(text)
   res <- retractcheck(dois)
 
   return(res) 
 }
 
-retractcheck_rtf <- function (path, ...) {
-  text <- textreadr::read_rtf(path, ...)
+#' Check rtf file for retractions
+#' 
+#' Check a rtf file for retractions.
+#' 
+#' @param path Path to rtf file to check
+#' 
+#' @return \code{\link{retractcheck}} dataframe
+#' @export
+#' @examples \dontrun{
+#'   retractcheck_rtf('manuscript.rtf')
+#' }
+
+retractcheck_rtf <- function (path) {
+  text <- textreadr::read_rtf(path)
   dois <- find_doi(text)
   res <- retractcheck(dois)
 
   return(res) 
 }
 
-retractcheck_html <- function (path, ...) {
-  text <- textreadr::read_html(path, ...)
+#' Check html file for retractions
+#' 
+#' Check a html file for retractions.
+#' 
+#' @param path Path to html file to check
+#' 
+#' @return \code{\link{retractcheck}} dataframe
+#' @export
+#' @examples \dontrun{
+#'   retractcheck_html('manuscript.html')
+#' }
+
+retractcheck_html <- function (path) {
+  text <- textreadr::read_html(path)
   dois <- find_doi(text)
   res <- retractcheck(dois)
 
--- R/utils.R
+++ R/utils.R
@@ -1,21 +1,55 @@
+#' Check for valid DOI
+#' 
+#' This helper function checks whether a DOI is valid. The regular expression
+#' is based on the one provided by CrossRef as providing the highest coverage
+#' (\url{https://bit.ly/doi-regex}).
+#' 
+#' @param doi Digital Object Identifier (string)
+#' 
+#' @return Boolean
+#' @export
+#' 
+#' @examples 
+#'   check_doi(doi = '10.1002/job.1787')
+#'   check_doi(doi = '10.1111/j.1365-2044.2012.07128.x')
+
 check_doi <- function (doi) {
   # bit.ly/doi-regex inspired
   regex <- '^10\\.\\d{4,9}/[-._;()/:A-Z0-9]+$'
   return(grepl(x = doi, pattern = regex,
      perl = TRUE, ignore.case = TRUE))
 }
-# '10.1111/j.1365-2044.2012.07128.x' = 404
-# '10.1002/job.1787' = 200
+
+#' OpenRetractions URL
+#' 
+#' Helper function to easily maintain the API calls to 
+#' \url{http://openretractions.com}
+#' 
+#' @param doi Digital Object Identifier (string)
+#' 
+#' @return URL (string)
 
 construct_url <- function (doi) {
   return(sprintf('http://openretractions.com/api/doi/%s/data.json',
       doi))
 }
 
+#' Find DOIs in strings
+#' 
+#' Helper function to find DOIs in strings. Can occasionally erroneously
+#' extract DOIs (subset of another DOI for example). Regular expression
+#' based on CrossRef (\url{http://bit.ly/doi-regex}; see also 
+#' \code{\link{check_doi}}).
+#' 
+#' @param strings Vector of strings to check for DOIs
+#' 
+#' @return Vector of DOIs
+#' @export
+#' 
+#' @examples 
+#'   find_doi('This contains 10.1111/j.1365-2044.2012.07128.x')
+
 find_doi <- function (strings) {
-  # bit.ly/doi-regex inspired
-  # Removed ^ and $ because it can happen
-  # Some issues with trailing semi-colons sometimes
   regex <- '10\\.\\d{4,9}/[-._;()/:A-Z0-9]+'
   doiLoc <- gregexpr(text = strings, pattern = regex,
      perl = TRUE, ignore.case = TRUE)
